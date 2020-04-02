abfcab685890e1571e8965c02826e45db07dd6b4
Daniel Saraiva <danniel.saraiva@gmail.com>
2020-04-01 23:05:59 -0300

    Limitado a lista para ver se resolve problema de renderização

--- src/views/Recent.vue
+++ src/views/Recent.vue
@@ -3,17 +3,16 @@
     <p>recent</p>
     <v-card class="mx-auto" max-width="500" dark>
       <v-container fluid v-for="jogo in listaJogos" :key="jogo.id">
-          
-            <v-card>
-              <v-img
-                :src="jogo.background"
-                class="white--text align-end"
-                gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-                height="200px"
-              >
-                <v-card-title v-text="jogo.nome"></v-card-title>
-              </v-img>
-            </v-card>
+        <v-card>
+          <v-img
+            :src="jogo.background"
+            class="white--text align-end"
+            gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+            height="200px"
+          >
+            <v-card-title v-text="jogo.nome"></v-card-title>
+          </v-img>
+        </v-card>
       </v-container>
     </v-card>
   </div>
@@ -39,18 +38,21 @@ export default {
           document.documentElement.offsetHeight;
 
         if (bottomOfWindow && !this.carregando) {
-          if(this.next == null) return;
-          this.carregando = true
+          if (this.next == null) return;
+          this.carregando = true;
+          if (this.listaJogos.length > 20) this.listaJogos.splice(0, 20);
           axios
-            .get(`https://arcadaweb.com.br/api/gamerelease/listagames.php?next=${this.next}`)
+            .get(
+              `https://arcadaweb.com.br/api/gamerelease/listagames.php?next=${this.next}`
+            )
             .then(response => {
               // JSON responses are automatically parsed.
-              this.next = response.data.next
+              this.next = response.data.next;
               response.data.retorno.forEach(element => {
-                this.listaJogos.push(element)
+                this.listaJogos.push(element);
               });
-              this.carregando = false
-              console.log(this.listaJogos)
+              this.carregando = false;
+              console.log(this.listaJogos);
             })
             .catch(e => {
               this.errors.push(e);
